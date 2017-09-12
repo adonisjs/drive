eba691b648f65cd43b68b1cc2e3c1d625c145c36
Harminder Virk <virk.officials@gmail.com>
2017-09-12 17:40:27 +0530

    fix(instructions): fix wrong path to config file

--- instructions.js
+++ instructions.js
@@ -14,7 +14,7 @@ const path = require('path')
 module.exports = async (cli) => {
   try {
     await cli.copy(
-      path.join(__dirname, '../examples/config.js'),
+      path.join(__dirname, './examples/config.js'),
       path.join(cli.helpers.configPath(), 'drive.js')
     )
     await cli.command.completed('create', 'config/drive.js')
