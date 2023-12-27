f28ca909e4864d58233b6b415374dcad2d9ce743
Harminder Virk <virk.officials@gmail.com>
2023-12-27 11:42:57 +0530

    refactor: prefix unused variables with _

--- configure.ts
+++ configure.ts
@@ -14,4 +14,4 @@
 
 import ConfigureCommand from '@adonisjs/core/commands/configure'
 
-export async function configure(command: ConfigureCommand) {}
+export async function configure(_command: ConfigureCommand) {}
